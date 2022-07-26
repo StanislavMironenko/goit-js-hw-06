b7c919a6e4fcc64d0664fdfdaf922b25023cf769
StanislavMironenko <98009389+StanislavMironenko@users.noreply.github.com>
2022-07-26 15:30:21 +0300

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -7,35 +7,31 @@ const destroyBtn = document.querySelector('button[data-destroy]');
 const galleryBoxes = document.querySelector('#boxes')
 
 
-  createBtn.addEventListener("click", createColection);
+  createBtn.addEventListener("click", createBoxes);
   destroyBtn.addEventListener("click", () => {
   galleryBoxes.innerHTML = '',
     input.value = ''
   })
 
 
-function createColection() {
-  let elWidth =20;
+function createBoxes() {
+  let elWidth = 20;
   let elHeight = 20;
-  
+
   const colectionElements = [];
   for (let i = 0; i < input.value; i += 1) {
     const ten = 10;
     elWidth += ten;
-    elHeight +=ten;
+    elHeight += ten;
     const div = document.createElement("div");
-      
-  
-      div.style.width = `${elWidth}px`;
-      div.style.height = `${elHeight}px`;
-      div.style.backgroundColor = getRandomHexColor();
-      
-      colectionElements.push(div);
-     
+
+    div.style.width = `${elWidth}px`;
+    div.style.height = `${elHeight}px`;
+    div.style.backgroundColor = getRandomHexColor();
+
+    colectionElements.push(div);
   }
- return galleryBoxes.append(...colectionElements);
- 
- 
+  return galleryBoxes.append(...colectionElements);
 }
 
 
